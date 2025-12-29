caabb1aae7ab88d92d55aca91b6161b24ce1eab9
fadrian06 <franyeradriansanchez@gmail.com>
2025-12-29 12:46:10 -0400

    composer remove vlucas/phpdotenv & composer require symfony/dotenv:^7.4 -W & composer require symfony/var-dumper:^7.4 --dev -W

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+(new Dotenv)->load(__DIR__ . '/../.env.example', __DIR__ . '/../.env');
 
 return [
     'database' => [
