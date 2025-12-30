1f43896b99a160ee0d0c61058b1adc314464e929
fadrian06 <franyeradriansanchez@gmail.com>
2025-12-29 22:30:32 -0400

    npm i @tailwindcss/cli@4.1.18 tailwindcss@4.1.18 -D

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -52,16 +52,8 @@ use RECHARGE\Enums\SessionKey;
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet" />
 
-    <!--
-        Tailwind CSS (CDN - Development Mode)
-        Note: "cdn.tailwindcss.com should not be used in production" warning is expected.
-        We use it for rapid MVP development.
-    -->
-    <script src="https://cdn.tailwindcss.com"></script>
-
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <script src="./index.js"></script>
     <link rel="stylesheet" href="./index.css" />
 </head>
 
